a2d8457e2e30f5794349d2d197fa239664510d52
Dmitriy <batka1488@gmail.com>
2018-01-12 11:59:39 +0300

    Full graph without computations.

--- task.py
+++ task.py
@@ -9,13 +9,15 @@ from Node import Node
 
 fname = "input1.txt"
 input_data = np.loadtxt(fname, dtype=None, delimiter='\t')
-init_modes = input_data[:, 0:-1]  # массив режимов (количество состояний на количество признаков)
+modes = input_data[:, 0:-1]  # массив режимов (количество состояний на количество признаков)
 probabilities = input_data[:, -1]  # массив вероятностей состояний
 states_list = list()
-init_test_set = list(range(1, len(init_modes[0]) + 1))  # множество проверок
-init_state_set = list(range(1, len(init_modes[:]) + 1))  # множество состояний
+init_test_set = list(range(1, len(modes[0]) + 1))  # множество проверок
+init_state_set = list(range(1, len(modes[:]) + 1))  # множество состояний
 dot = Digraph(comment='Граф состояний')
 node_number = 0
+
+G = nx.Graph()
 
 
 def check_modes_values(data):  # определение количества различных режимов
@@ -70,43 +72,43 @@ def the_first_modes_separation(modes):
                 states_list.append(node)
 
 
-def mode_separation(modes, test_set, state_set):
+def mode_separation(parent_node, test_list, state_set):
     global node_number
     mode_set = check_modes_values(modes)  # определение количества режимов
-    custom_test_set = delete_list(init_test_set.copy(), test_set)
-    for j in custom_test_set:  # разделение на группы состояний по режимам по каждому признаку
-        d = dict()
+    custom_test_list = delete_list(init_test_set.copy(), test_list) # проверки, которые ещё не проводились
+    for j in custom_test_list:  # разделение на группы состояний по режимам по каждому признаку
+        d = dict() # состояния, разделённые по режимам
         for m in mode_set:
             d[m] = set()
         for i in state_set:
             d[modes[i - 1, j - 1]].add(i)
-        t_set = test_set.copy()
-        # t_set.remove(j)
-        t_set.append(j)
-        # test_set = t_set
-        for v in d.values():
-            if len(v) == 0:
+        t_list = test_list.copy()
+        t_list.append(j)
+        for state in d.values():
+            if len(state) == 0:
                 continue
-            if len(t_set) < 6 and len(v) > 1:
-                node = Node(v, t_set)
-                node_number = node_number + 1
-                print(node_number, '-',state_set_to_string(v))
-                dot.edge(str(node_number), state_set_to_string(v))
+            if len(t_list) < 6 and len(state) > 1:
+                node = Node(state, t_list)
 
-                # print(node.state_list, node.modes)
-                # tmp_set = init_test_set.copy()
-                # tmp_set = delete_list(tmp_set, t_set)
-                # print(tmp_set, v)
-                print(t_set, v)
+                dot.node(str(hash(node)), state_set_to_string(state))
+                dot.edge(str(hash(parent_node)), str(hash(node)), str(j), constraint='true')
+
+                G.add_node(str(hash(node)))
+                G.add_edge(str(hash(parent_node)), str(hash(node)))
+
+                print(t_list, state)
                 states_list.append(node)
-                mode_separation(modes, t_set, v)
+                mode_separation(node, t_list, state)
             else:
-                node = Node(v, t_set)
-                # print(node.state_list, node.modes)
-                # tmp_set = init_test_set.copy()
-                # tmp_set = delete_list(tmp_set, t_set)
-                # print(tmp_set, v)
-                print(t_set, v)
+                node = Node(state, t_list)
+
+                dot.node(str(hash(node)), state_set_to_string(state))
+                dot.edge(str(hash(parent_node)), str(hash(node)), str(j), constraint='true')
+
+                G.add_node(str(hash(node)))
+                G.add_edge(str(hash(parent_node)), str(hash(node)))
+
+                print(t_list, state)
                 states_list.append(node)
 
 
@@ -141,16 +143,20 @@ def formula(state):  # вычисление значений по формула
     print(summa)
 
 
-# the_first_modes_separation(init_modes)
-mode_separation(init_modes, list(), init_state_set)
+start_node = Node(init_state_set, list())
+print(str(hash(start_node)), '-',state_set_to_string(init_state_set))
+dot.node(str(hash(start_node)), state_set_to_string(init_state_set))
+
+mode_separation(start_node, list(), init_state_set)
 print(len(states_list))
-# print(np.array(states))
+# dot.format = 'png'
+dot.render('test-output/round-table.gv', view=True)
+
 # for s in states_list:
 #     formula(s)
 
 
-# G = nx.Graph()
-# nx.draw(G,pos=nx.spectral_layout(G), nodecolor='r',edge_color='b')
+# nx.draw(G,pos=nx.spring_layout(G), nodecolor='r',edge_color='b')
 # plt.figure(figsize=(8, 6))
 # nx.draw(g, pos=node_positions, edge_color=edge_colors, node_size=10, node_color='black')
 # plt.title('Graph Representation of Sleeping Giant Trail Map', size=15)
